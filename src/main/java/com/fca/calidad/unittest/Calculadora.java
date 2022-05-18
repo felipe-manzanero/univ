224a233c61c1d83be58ab8d3c5bd9a0ddfc5c8eb
felipe-manzanero <86502958+felipe-manzanero@users.noreply.github.com>
2022-05-18 10:42:08 -0500

    Add files via upload

---
+++ src/main/java/com/fca/calidad/unittest/Calculadora.java
@@ -0,0 +1,30 @@
+package com.fca.calidad.unittest;
+
+public class Calculadora {
+	
+		private double ultimoResultado;
+		
+		double suma(double primerSumando, double segundoSumando) {
+			return ultimoResultado=primerSumando+segundoSumando;
+		}
+		
+		private double resta(double minuendo, double sustraendo) {
+			return ultimoResultado=minuendo-sustraendo;
+		}
+		
+		private double multiplicacion(double primerFactor, double segundoFactor) {
+			return ultimoResultado=primerFactor*segundoFactor;
+		}
+		
+		double division(double dividendo, double divisor)throws ArithmeticException  {
+			if(divisor==0) {
+				throw new ArithmeticException();
+			}else
+			return ultimoResultado=dividendo/divisor;
+		}
+		
+		private double getUltimoResultado() {
+			return ultimoResultado;
+		}
+		
+	}
